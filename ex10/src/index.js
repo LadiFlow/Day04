01b58d0d4578262b0ecc6fcaa83aa606c1f82371
LadiFlow <adi_curt@hotmail.com>
2021-03-11 17:20:57 +0100

    Declare string variables

---
+++ ex10/src/index.js
@@ -0,0 +1,18 @@
+function main(){
+    // Only change code bellow this line
+    var myFirstName;
+    var myLastName;
+    var myHomeTown;
+
+     myFirstName = Adi;
+     myLastName = Curt;
+     myHomeTown = Sarajevo;
+    // Only change code above this line
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+  }
+  console.log(main()); 
+  module.exports = main;
