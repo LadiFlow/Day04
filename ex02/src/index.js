04733d373505f774c91603e55a3dadf0c22295e4
LadiFlow <adi_curt@hotmail.com>
2021-03-11 12:26:25 +0100

    Added index.js

---
+++ ex02/src/index.js
@@ -0,0 +1,11 @@
+function main(){
+var myRes = 75;
+// only change code below this line
+var myres;
+myRes = 76++;
+// only change code above this line
+return myRes;
+}
+
+console.log(main());
+module.exports = main;
