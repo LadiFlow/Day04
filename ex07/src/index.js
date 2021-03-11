3dc0931c2819ac2d3b0c41e50ae808a4a6f99359
LadiFlow <adi_curt@hotmail.com>
2021-03-11 16:27:33 +0100

    Finding a remainder

---
+++ ex07/src/index.js
@@ -0,0 +1,12 @@
+function main(numOne, numTwo) {
+    // Only change code bellow this line
+    var numOne;
+    var numTwo;
+    reminder = numOne % numTwo;
+    // Only change code above this line
+    return reminder;
+  }
+  console.log(main(5, 2)); // change this line
+  console.log(main(15, 4)); // change this line
+  console.log(main(102, 25)); // change this line
+  module.exports = main;
