8dc5ab650746bf402042099c8adeb3c0e9cbb891
LadiFlow <adi_curt@hotmail.com>
2021-03-11 13:26:06 +0100

    Added a scientific notation

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -3,12 +3,13 @@ function main(){
     var myDecimal;
     var myDecimalE;
     myDecimal = 2.4;
-    myDeciamalE = 2.54 × 10^2;
-
+    myDecimalE = 0.000254e+1;
     // only change code above this line
-    return myDecimal;
-    return myDecimalE;
-    }
+    return {
+        myDecimal,
+        myDecimalE
+    };
+    };
     
     console.log(main());
     module.exports = main;
