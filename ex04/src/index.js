fd0f3ee373dd0dfaeca2885fbeb5e7488dc355b0
LadiFlow <adi_curt@hotmail.com>
2021-03-11 12:56:43 +0100

    Added a number with a scientific exponent

---
+++ ex04/src/index.js
@@ -0,0 +1,14 @@
+function main(){
+    // only change code below this line
+    var myDecimal;
+    var myDecimalE;
+    myDecimal = 2.4;
+    myDeciamalE = 2.54 × 10^2;
+
+    // only change code above this line
+    return myDecimal;
+    return myDecimalE;
+    }
+    
+    console.log(main());
+    module.exports = main;
